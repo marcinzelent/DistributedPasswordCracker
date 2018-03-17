7e2a8dbd051b94115672cf22cc66ef09ba8376ca
marcinzelent <zelent.marcin@gmail.com>
2018-03-17 15:30:15 +0100

    .NET Core is cool!

---
+++ DistributedPasswordCracker/DistributedPasswordCracker.Client/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using DistributedPasswordCracker.Client.Models;
+
+namespace DistributedPasswordCracker.Client
+{
+    class Program
+    {
+        const int NumberOfTasks = 5;
+
+        private static StreamReader sr;
+        private static StreamWriter sw;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Connecting to server... ");
+            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            NetworkStream ns = clientSocket.GetStream();
+            Console.Write("OK\n");
+            sr = new StreamReader(ns);
+            sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            Console.Write("Getting data from the server... ");
+            string data = GetData();
+            if (data != "") Console.Write("OK\n");
+
+            Console.Write("Parsing data... ");
+            string dictionary = ParseData(data);
+            if (dictionary != "") Console.Write("OK\n");
+
+            Console.Write("Splitting dictionary... ");
+            string[,] splitDictionary = SplitDictionary(dictionary);
+            if (splitDictionary[0,0] != "") Console.Write("OK\n");
+
+            List<UserInfoClearText> result = new List<UserInfoClearText>();
+            for(int i = 0; i < NumberOfTasks; i++)
+            {
+                Console.Write("Running task no." + (i + 1) + "...\n" );
+                int rowLength = splitDictionary.GetLength(1);
+                string[] chunk = new string[rowLength];
+                for (int j = 0; j < rowLength; j++)
+                    chunk[j] = splitDictionary[i, j];
+                result = DecryptPassword(chunk);
+            }
+            if (result.Count != 0)
+            {
+                sw.WriteLine(result[0].ToString());
+            }
+            else
+            {
+                sw.WriteLine("Nothing Found!");
+            }
+        }
+
+        private static string GetData()
+        {
+            string data = "";
+
+            while (true)
+            {
+                string message = sr.ReadLine();
+                if (message != "") data += message + "\n";
+                else break;
+            }
+
+            return data;
+        }
+
+        private static string ParseData(string data)
+        {
+            var splitData = data.Split('\n');
+
+            if (splitData[0] == "DPCP 1.0")
+            {
+                splitData[1] = splitData[1].Replace('|', '\n');
+                File.WriteAllText("passwords.txt", splitData[1]);
+                File.WriteAllText("dictionary.txt", splitData[2]);
+            }
+
+            return splitData[2];
+        }
+
+        private static string[,] SplitDictionary(string dictionary)
+        {
+            string[] dicWords = dictionary.Split('|');
+            int dicWordsLength = dicWords.Length;
+            string[,] splitDictionary = new string[NumberOfTasks,dicWordsLength/NumberOfTasks];
+            int offset = 0;
+            int j = 0;
+
+            for (int i = 0; i < NumberOfTasks; i++)
+            {
+                while (j < dicWordsLength/NumberOfTasks)
+                {
+                    splitDictionary[i, j] = dicWords[j + offset];
+                    j++;
+                }
+                offset += dicWordsLength/NumberOfTasks;
+                j = 0;
+            }
+
+            return splitDictionary;
+        }
+
+        private static List<UserInfoClearText> DecryptPassword(string[] dictionary)
+        {
+            Cracking cracker = new Cracking();
+            var result = cracker.RunCracking(dictionary);
+
+            return result;
+}
+    }
+}
---
+++ DistributedPasswordCracker/DistributedPasswordCracker.Server/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using DistributedPasswordCracker.Server.Models;
+using DistributedPasswordCracker.Server.Utilities;
+
+namespace DistributedPasswordCracker.Server
+{
+    class Program
+    {
+        static int NumberOfClients = 1;
+
+        static void Main(string[] args)
+        {
+            List<UserInfo> userInfos = PasswordFileHandler.ReadPasswordFile("passwords.txt");
+            List<ClientConnection> clients = new List<ClientConnection>();
+            List<string> allChunks =  new List<string>();
+
+            string users = "";
+            foreach (UserInfo u in userInfos)
+                users += u + "|";
+
+            var dictionary = File.ReadAllText("dictionary.txt");
+            dictionary = dictionary.Replace('\n', '|');
+            var splitDictionary = SplitDictionary(dictionary);
+            for (int i = 0; i < splitDictionary.GetLength(0); i++)
+            {
+                StringBuilder chunk = new StringBuilder();
+                for (int j = 0; j < splitDictionary.GetLength(1); j++)
+                {
+                    chunk.Append(splitDictionary[i, j]);
+                    chunk.Append('|');
+                }
+                allChunks.Add(chunk.ToString());
+            }
+
+            IPAddress ip = IPAddress.Any;
+            TcpListener serversocket = new TcpListener(ip, 6789);
+            
+            serversocket.Start();
+            Console.WriteLine("Server started.");
+            int counter = 0;
+            while (true)
+            {
+                if (clients.Count == NumberOfClients)
+                {
+                    string[] returnedResult = RunAsync(clients).Result;
+                    foreach (string s in returnedResult)
+                        Console.WriteLine(s);
+                }
+                else
+                {
+                    TcpClient connectionSocket = serversocket.AcceptTcpClient();
+                    Console.WriteLine("Client connected.");
+
+                    clients.Add(new ClientConnection(connectionSocket, allChunks[counter], users));
+                    counter++;
+                }
+            }
+            serversocket.Stop();
+        }
+
+        private static string[,] SplitDictionary(string dictionary)
+        {
+            string[] dicWords = dictionary.Split('|');
+            int dicWordsLength = dicWords.Length;
+            string[,] splitDictionary = new string[NumberOfClients, dicWordsLength / NumberOfClients];
+            int offset = 0;
+            int j = 0;
+
+            for (int i = 0; i < NumberOfClients; i++)
+            {
+                while (j < dicWordsLength / NumberOfClients)
+                {
+                    splitDictionary[i, j] = dicWords[j + offset];
+                    j++;
+                }
+                offset += dicWordsLength / NumberOfClients;
+                j = 0;
+            }
+
+            return splitDictionary;
+        }
+
+        public static async Task<string[]> RunAsync(List<ClientConnection> clients)
+        {
+            List<Task<string>> tasks = new List<Task<string>>();
+            for (int i = 0; i < NumberOfClients; i++)
+                tasks[i] = clients[i].SendToClient();
+
+            string[] result = await Task.WhenAll(tasks);
+
+            /*
+            var C1 = await C1task;
+            var C2 = await C2task;
+            var C3 = await C3task;
+            var C4 = await C4task;
+            var C5 = await C5task;
+            */
+            return result;
+        }
+
+    }
+}
